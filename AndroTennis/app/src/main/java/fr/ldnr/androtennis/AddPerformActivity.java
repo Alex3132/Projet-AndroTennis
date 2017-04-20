eef57d418a0f50a9be6fc802929e0c7c014da50e
jean.mariette <jean.mariette.pro@gmail.com>
2017-04-20 11:15:43 +0200

    Ajout de strings menu principal
    ajout activité ajout performances

---
+++ AndroTennis/app/src/main/java/fr/ldnr/androtennis/AddPerformActivity.java
@@ -0,0 +1,17 @@
+package fr.ldnr.androtennis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Philip on 20/04/2017.
+ */
+
+public class AddPerformActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
