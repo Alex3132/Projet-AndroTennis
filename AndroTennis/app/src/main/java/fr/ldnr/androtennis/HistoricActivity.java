951fadd6409013173f08797bb4746b2b13e14541
jean.mariette <jean.mariette.pro@gmail.com>
2017-04-20 11:04:45 +0200

    ajout des menus dans les activités

--- AndroTennis/app/src/main/java/fr/ldnr/androtennis/HistoricActivity.java
+++ AndroTennis/app/src/main/java/fr/ldnr/androtennis/HistoricActivity.java
@@ -3,6 +3,7 @@ package fr.ldnr.androtennis;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
 
 /**
  * Created by Philip on 20/04/2017.
@@ -12,5 +13,11 @@ public class HistoricActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
     }
 }
