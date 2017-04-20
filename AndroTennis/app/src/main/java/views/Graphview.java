41181249dc450339f9745a89202abdfad042b422
jean.mariette <jean.mariette.pro@gmail.com>
2017-04-20 11:08:03 +0200

    renommé HistoricView en GraphView

---
+++ AndroTennis/app/src/main/java/views/Graphview.java
@@ -0,0 +1,16 @@
+package views;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Philip on 20/04/2017.
+ */
+
+public class Graphview extends View {
+    public Graphview(Context context) {
+        super(context);
+
+    }
+
+}
