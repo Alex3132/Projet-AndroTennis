4e6bde0ac6d1c7f2b807d7c79a54c3c85da34322
jean.mariette <jean.mariette.pro@gmail.com>
2017-04-23 00:46:53 +0200

    Export XML Terminé

--- AndroTennis/app/src/main/java/fr/ldnr/androtennis/HomeActivity.java
+++ AndroTennis/app/src/main/java/fr/ldnr/androtennis/HomeActivity.java
@@ -1,5 +1,6 @@
 package fr.ldnr.androtennis;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -7,14 +8,23 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.util.Xml;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
 import misc.MenuNavigation;
@@ -30,7 +40,8 @@ public class HomeActivity extends Activity {
 
     //perform ids
 
-    ArrayList<String> ids = new ArrayList<String>();
+    private ArrayList<String> ids = new ArrayList<String>();
+    public String bddFileName = "bddAndrotennis.xml";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,8 +49,9 @@ public class HomeActivity extends Activity {
 
 
         setContentView(R.layout.home);
-
-
+        if (shouldAskPermissions()) {
+            askPermissions();
+        }
 
         //for api 11 minimum
         View home = findViewById(R.id.home_buttons);
@@ -52,7 +64,6 @@ public class HomeActivity extends Activity {
 
         setLastAddViews();
     }
-
 
 
     @Override
@@ -77,6 +88,8 @@ public class HomeActivity extends Activity {
                 return true;
 
             case R.id.home_menu_export:
+
+                onExportButtonClicked(findViewById(R.id.home_menu_export));
                 /*TODO : Add export code */
                 return true;
 
@@ -115,7 +128,129 @@ public class HomeActivity extends Activity {
 
     public void onExportButtonClicked(View view) {
 
-        /*TODO: Add export code*/
+        String xmlToWrite = getDatabaseXml();
+
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+
+            try {
+if(Build.VERSION.SDK_INT<19){
+                File dir = new File(Environment.getExternalStorageDirectory()+"/Documents");
+                Boolean isPresent = true;
+
+                if(!dir.exists()) {
+                    isPresent = dir.mkdirs();
+                }
+
+                if(isPresent) {
+                    File f = new File(dir.getAbsolutePath(), bddFileName);
+                    FileOutputStream fw = new FileOutputStream(f, true);
+
+                    fw.write(xmlToWrite.getBytes());
+                    fw.close();
+                    Log.i("Données Exportées", "Exporté : "+Environment.getExternalStorageDirectory()+"/Documents");
+                }}else{
+
+                    File dir2 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+                    dir2.mkdirs();
+
+                    File f = new File(dir2, bddFileName);
+                    FileOutputStream fw = new FileOutputStream(f, true);
+
+                    fw.write(getDatabaseXml().getBytes());
+                    fw.close();
+                    Log.i("Données Exportées", "Exporté api 19 : "+Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS));
+
+                }
+                } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+         catch (IOException ioexc) {
+
+                Toast.makeText(this, "Physical Storage Error", Toast.LENGTH_LONG).show();
+                Log.e("HomeActivity", "SDCard Error", ioexc);
+            }
+
+        } else {
+
+            Log.e("HomeActivity", "noSDcard");
+        }
+    }
+
+
+    public String getDatabaseXml() {
+
+
+        XmlSerializer serializer = Xml.newSerializer();
+
+        StringWriter writer = new StringWriter();
+
+        try {
+
+            manageBdd manager = new manageBdd(this);
+
+            SQLiteDatabase db = manager.getReadableDatabase();
+
+            Cursor c = db.rawQuery("SELECT * from donnees", null);
+
+            int number = c.getColumnCount();
+
+            if (c.moveToFirst()) {
+                serializer.setOutput(writer);
+                serializer.startDocument("UTF-8", true);
+                serializer.startTag("", "Columns");
+                serializer.attribute("", "number", Integer.toString(number));
+                String[] columns = c.getColumnNames();
+
+                for (String str : columns) {
+
+                    serializer.text(str);
+
+                }
+                serializer.endTag("", "Columns");
+
+                for (int i = 0; i < number; i++) {
+                    serializer.startTag("", c.getColumnName(i));
+                    serializer.text(c.getString(i));
+                    serializer.endTag("", c.getColumnName(i));
+
+
+                }
+
+                while (c.moveToNext()) {
+
+                    for (int i = 0; i < number; i++) {
+                        serializer.startTag("", c.getColumnName(i));
+                        serializer.text(c.getString(i));
+                        serializer.endTag("", c.getColumnName(i));
+
+
+                    }
+
+                }
+
+
+            }
+            serializer.endDocument();
+            c.close();
+            db.close();
+
+            return writer.toString();
+
+
+        } catch (SQLiteException E) {
+
+            Log.i("Erreur SQL", "Erreur : " + E.getMessage());
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        /*TODO: verify export code*/
+
+
     }
 
     public void setLastAddViews() {
@@ -135,122 +270,123 @@ public class HomeActivity extends Activity {
         lastAdd5.setVisibility(GONE);
 
 
-        try{
+        try {
 
-        manageBdd manager = new manageBdd(this);
+            manageBdd manager = new manageBdd(this);
 
-        SQLiteDatabase db = manager.getReadableDatabase();
+            SQLiteDatabase db = manager.getReadableDatabase();
 
-        Cursor curs = db.rawQuery("SELECT count(*) noma from donnees", null);
+            Cursor curs = db.rawQuery("SELECT count(*) noma from donnees", null);
 
-        if (curs.moveToFirst()) {
-            nb = curs.getInt(0);
-            Log.i("NOmbre : ", Integer.toString(nb));
-            curs.close();
+            if (curs.moveToFirst()) {
+                nb = curs.getInt(0);
+                Log.i("NOmbre : ", Integer.toString(nb));
+                curs.close();
 
-        } else {
+            } else {
 
-            nb = 0;
-        }
+                nb = 0;
+            }
 
-        Cursor c = db.rawQuery("SELECT id, noma, dateTime from donnees ORDER BY dateTime DESC LIMIT 5", null);
+            Cursor c = db.rawQuery("SELECT id, noma, dateTime from donnees ORDER BY dateTime DESC LIMIT 5", null);
 
-        if (c.moveToFirst()) {
+            if (c.moveToFirst()) {
 
-            ids.add(c.getString(c.getColumnIndex("id")));
-            tabBack.add(getResources().getString(R.string.home_adversary) +c.getString(c.getColumnIndex("noma")) +getResources().getString(R.string.home_date)+ c.getString(c.getColumnIndex("dateTime")).substring(0,10));
-
-            while (c.moveToNext()) {
                 ids.add(c.getString(c.getColumnIndex("id")));
-                tabBack.add(getResources().getString(R.string.home_adversary) +c.getString(c.getColumnIndex("noma")) +getResources().getString(R.string.home_date)+ c.getString(c.getColumnIndex("dateTime")).substring(0,10));
+                tabBack.add(getResources().getString(R.string.home_adversary) + c.getString(c.getColumnIndex("noma")) + getResources().getString(R.string.home_date) + c.getString(c.getColumnIndex("dateTime")).substring(0, 10));
 
+                while (c.moveToNext()) {
+                    ids.add(c.getString(c.getColumnIndex("id")));
+                    tabBack.add(getResources().getString(R.string.home_adversary) + c.getString(c.getColumnIndex("noma")) + getResources().getString(R.string.home_date) + c.getString(c.getColumnIndex("dateTime")).substring(0, 10));
+
+                }
+                c.close();
+
+                switch (nb) {
+
+                    case 0:
+                        break;
+                    case 1:
+                        lastAdd1.setText(tabBack.get(0));
+
+                        break;
+                    case 2:
+
+                        lastAdd2.setVisibility(View.VISIBLE);
+                        lastAdd1.setText(tabBack.get(0));
+                        lastAdd2.setText(tabBack.get(1));
+                        break;
+                    case 3:
+
+                        lastAdd2.setVisibility(View.VISIBLE);
+                        lastAdd3.setVisibility(View.VISIBLE);
+                        lastAdd1.setText(tabBack.get(0));
+                        lastAdd2.setText(tabBack.get(1));
+                        lastAdd3.setText(tabBack.get(2));
+                        break;
+                    case 4:
+
+                        lastAdd2.setVisibility(View.VISIBLE);
+                        lastAdd3.setVisibility(View.VISIBLE);
+                        lastAdd4.setVisibility(View.VISIBLE);
+                        lastAdd1.setText(tabBack.get(0));
+                        lastAdd2.setText(tabBack.get(1));
+                        lastAdd3.setText(tabBack.get(2));
+                        lastAdd4.setText(tabBack.get(3));
+                        break;
+
+                    default:
+                        lastAdd2.setVisibility(View.VISIBLE);
+                        lastAdd3.setVisibility(View.VISIBLE);
+                        lastAdd4.setVisibility(View.VISIBLE);
+                        lastAdd5.setVisibility(View.VISIBLE);
+                        lastAdd1.setText(tabBack.get(0));
+                        lastAdd2.setText(tabBack.get(1));
+                        lastAdd3.setText(tabBack.get(2));
+                        lastAdd4.setText(tabBack.get(3));
+                        lastAdd5.setText(tabBack.get(4));
+                }
             }
-            c.close();
+            db.close();
+        } catch (SQLiteException E) {
 
-            switch (nb) {
-
-                case 0:
-                    break;
-                case 1:
-                    lastAdd1.setText(tabBack.get(0));
-
-                    break;
-                case 2:
-
-                    lastAdd2.setVisibility(View.VISIBLE);
-                    lastAdd1.setText(tabBack.get(0));
-                    lastAdd2.setText(tabBack.get(1));
-                    break;
-                case 3:
-
-                    lastAdd2.setVisibility(View.VISIBLE);
-                    lastAdd3.setVisibility(View.VISIBLE);
-                    lastAdd1.setText(tabBack.get(0));
-                    lastAdd2.setText(tabBack.get(1));
-                    lastAdd3.setText(tabBack.get(2));
-                    break;
-                case 4:
-
-                    lastAdd2.setVisibility(View.VISIBLE);
-                    lastAdd3.setVisibility(View.VISIBLE);
-                    lastAdd4.setVisibility(View.VISIBLE);
-                    lastAdd1.setText(tabBack.get(0));
-                    lastAdd2.setText(tabBack.get(1));
-                    lastAdd3.setText(tabBack.get(2));
-                    lastAdd4.setText(tabBack.get(3));
-                    break;
-
-                default:
-                    lastAdd2.setVisibility(View.VISIBLE);
-                    lastAdd3.setVisibility(View.VISIBLE);
-                    lastAdd4.setVisibility(View.VISIBLE);
-                    lastAdd5.setVisibility(View.VISIBLE);
-                    lastAdd1.setText(tabBack.get(0));
-                    lastAdd2.setText(tabBack.get(1));
-                    lastAdd3.setText(tabBack.get(2));
-                    lastAdd4.setText(tabBack.get(3));
-                    lastAdd5.setText(tabBack.get(4));
-            }
-        }
-        db.close();}catch (SQLiteException E){
-
-            Log.i("Erreur SQL", "Erreur : "+E.getMessage());
+            Log.i("Erreur SQL", "Erreur : " + E.getMessage());
         }
     }
 
-    public void OnPerformClicked(View view){
+    public void OnPerformClicked(View view) {
 
         Intent intent = new Intent(HomeActivity.this, PerformShowActivity.class);
 
-        switch(view.getId()){
+        switch (view.getId()) {
 
             case R.id.last_add_1:
 
-                if(!ids.get(0).isEmpty()) {
+                if (!ids.get(0).isEmpty()) {
                     intent.putExtra("id", ids.get(0));
                     startActivityForResult(intent, 0);
                 }
                 break;
             case R.id.last_add_2:
-                if(!ids.get(1).isEmpty()) {
+                if (!ids.get(1).isEmpty()) {
                     intent.putExtra("id", ids.get(1));
                     startActivityForResult(intent, 0);
                 }
                 break;
             case R.id.last_add_3:
-                if(!ids.get(2).isEmpty()) {
+                if (!ids.get(2).isEmpty()) {
                     intent.putExtra("id", ids.get(2));
                     startActivityForResult(intent, 0);
                 }
                 break;
             case R.id.last_add_4:
-                if(!ids.get(3).isEmpty()) {
+                if (!ids.get(3).isEmpty()) {
                     intent.putExtra("id", ids.get(3));
                     startActivityForResult(intent, 0);
                 }
                 break;
             case R.id.last_add_5:
-                if(!ids.get(4).isEmpty()) {
+                if (!ids.get(4).isEmpty()) {
                     intent.putExtra("id", ids.get(4));
                     startActivityForResult(intent, 0);
                 }
@@ -271,6 +407,20 @@ public class HomeActivity extends Activity {
     protected void onRestart() {
         super.onRestart();
         setLastAddViews();
+    }
+
+    protected boolean shouldAskPermissions() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    @TargetApi(23)
+    protected void askPermissions() {
+        String[] permissions = {
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE"
+        };
+        int requestCode = 200;
+        requestPermissions(permissions, requestCode);
     }
 }
 
